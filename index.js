3319c1807fe9c12ef210fabdc7de1386fdcd2dd3
Shalov <batirhan28@inbox.ru>
2023-05-15 14:55:13 +0600

    npm init -y

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-abce
+while (true) {
+   let result = prompt('Введите число больше 100', 100);
+   if (result <= 100 ) {
+    alert('Введите другое число');
+   } else {
+    alert('Вы ввели верное число');
+    break;
+   }
+}
